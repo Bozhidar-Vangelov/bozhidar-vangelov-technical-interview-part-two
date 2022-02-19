64e165a9611cec3855f72c95de0d4b49fd60b15c
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-02-19 19:49:55 +0200

    Create a single name component

---
+++ src/components/Names/Name.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import { Card } from 'react-bootstrap';
+import Profile from './Profile/Profile.js';
+
+function Name({ user }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen((prevState) => !prevState);
+  return (
+    <>
+      <Card.Header onClick={toggle}>{user.name}</Card.Header>
+      <Profile userId={user.id} isOpen={isOpen} />
+    </>
+  );
+}
+
+export default Name;
--- src/components/Names/Names.js
+++ src/components/Names/Names.js
@@ -1,57 +1,16 @@
-import { useEffect, useState } from 'react';
 import { Container, Card } from 'react-bootstrap';
-import Profile from './Profile/Profile.js';
 
-import api from '../../services/usersService.js';
-import { useSelector, useDispatch } from 'react-redux';
-import { get } from '../../features/user.js';
+import Name from './Name.js';
 
 function Names() {
-  const [isOpen, setIsOpen] = useState(true);
-  const dispatch = useDispatch();
-
-  const user = useSelector((state) => state.user.value);
-
-  useEffect(() => {
-    (async () => {
-      try {
-        let newUsers = await api.get();
-        dispatch(get(newUsers.data));
-      } catch (err) {
-        console.log(err);
-      }
-    })();
-  }, [dispatch]);
-
   const names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-  const toggle = () => {
-    setIsOpen(!isOpen);
-    console.log(isOpen);
-  };
-
-  console.log(user);
-
-  const list = (
-    <>
-      <Card.Header fluid onClick={toggle}>
-        Bret
-      </Card.Header>
-      <Profile isOpen={isOpen} />
-      <button
-        onClick={() => {
-          console.log(user);
-        }}
-      >
-        AAAAAAAAAAAAAAAAA
-      </button>
-    </>
-  );
 
   return (
     <Container fluid id='names' className='m-0 p-0'>
       <Card id='card' fluid>
-        {names.map(() => list)}
+        {names.map(() => (
+          <Name key={user.id} user={user} />
+        ))}
       </Card>
     </Container>
   );
