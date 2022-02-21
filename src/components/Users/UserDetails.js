3dc09915bd326371a779b2fe10de14625d826925
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-02-22 01:22:11 +0200

    Fix get users' posts logic

--- src/components/Users/UserDetails.js
+++ src/components/Users/UserDetails.js
@@ -1,31 +1,28 @@
 import { useState } from 'react';
-
 import { Card, Button, Container } from 'react-bootstrap';
-import ProfileItemsList from './UserData.js';
+import UserData from './UserData.js';
 import PostsItemsList from '../Posts/PostsItemsList.js';
 
 const UserDetails = ({ isOpen, userId }) => {
-  const [skip, setSkip] = useState(true);
+  const [fetchPosts, setFetchPosts] = useState(true);
   const [showPosts, setShowPosts] = useState(false);
 
   const fetchUserPosts = () => {
     setShowPosts((prevState) => !prevState);
-    setSkip(false);
+    setFetchPosts(false);
   };
 
   return (
     <Card.Body className={isOpen ? 'p-0 mt-3' : 'd-none p-0 mt-3'}>
-      <ProfileItemsList userId={userId} fetchUserPosts={fetchUserPosts} />
+      <UserData userId={userId} fetchUserPosts={fetchUserPosts} />
       <Container className='text-center mt-5 mb-5'>
         <Button variant='primary' onClick={fetchUserPosts}>
           Get Users' posts
         </Button>
       </Container>
 
-      {showPosts ? (
-        <PostsItemsList userId={userId} shouldFetchPosts={skip} />
-      ) : (
-        ''
+      {showPosts && (
+        <PostsItemsList userId={userId} shouldFetchPosts={fetchPosts} />
       )}
     </Card.Body>
   );
