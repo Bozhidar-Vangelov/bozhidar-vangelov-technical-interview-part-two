bc0c60c70779225bbea25508849db11fe624fe25
Bozhidar-Vangelov <bozhidar.vangelov@gmail.com>
2022-02-22 01:40:08 +0200

    Handle post creation

--- src/components/Posts/PostsItemsList.js
+++ src/components/Posts/PostsItemsList.js
@@ -5,31 +5,55 @@ import {
   InputGroup,
   FormControl,
 } from 'react-bootstrap';
-import PostItem from './PostItem.js';
-
 import {
   useCreatePostMutation,
   useGetUserPostsQuery,
 } from '../../features/user.js';
 import { useState } from 'react';
+import PostItem from './PostItem.js';
 
 const PostsItemsList = ({ userId, shouldFetchPosts }) => {
   const { data: posts, isLoading } = useGetUserPostsQuery(userId, {
     skip: shouldFetchPosts,
   });
-
   const [createPost] = useCreatePostMutation();
-
   const [show, setShow] = useState(false);
+  const [hasChanged, setHasChanged] = useState({
+    title: false,
+    body: false,
+  });
+  const [newPost, setNewPost] = useState({
+    title: '',
+    body: '',
+  });
 
-  const handleClose = async () => {
+  const handleShow = () => setShow(true);
+  const handleClose = async () => setShow(false);
+  const handleChange = (e) => {
+    setHasChanged((prevState) => ({
+      ...prevState,
+      [e.target.name]: true,
+    }));
+    setNewPost((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const handleCreate = async () => {
+    if (!newPost.title || !newPost.body) {
+      setHasChanged({
+        title: true,
+        body: true,
+      });
+
+      return;
+    }
     await createPost({
       userId,
+      newPost,
     });
-
     setShow(false);
   };
-  const handleShow = () => setShow(true);
 
   if (isLoading || !posts) {
     return 'Loading';
@@ -52,18 +76,28 @@ const PostsItemsList = ({ userId, shouldFetchPosts }) => {
         <Modal.Body>
           <InputGroup className='mb-3'>
             <InputGroup.Text>Title</InputGroup.Text>
-            <FormControl name='title' />
+            <FormControl
+              name='title'
+              isInvalid={hasChanged.title && !newPost.title}
+              value={newPost.title}
+              onChange={handleChange}
+            />
           </InputGroup>
           <InputGroup className='mb-3'>
             <InputGroup.Text>Body</InputGroup.Text>
-            <FormControl name='body' />
+            <FormControl
+              name='body'
+              isInvalid={hasChanged.body && !newPost.body}
+              value={newPost.body}
+              onChange={handleChange}
+            />
           </InputGroup>
         </Modal.Body>
         <Modal.Footer>
           <Button variant='secondary' onClick={handleClose}>
             Close
           </Button>
-          <Button variant='primary' onClick={handleClose}>
+          <Button variant='primary' onClick={handleCreate}>
             Save Post
           </Button>
         </Modal.Footer>
